1e9e308e0e5506bd741cac5ddf176b122adabd72
kairosity <karinafinegan@gmail.com>
2020-08-27 12:00:46 +0000

    Pass first test

---
+++ scripts/drink-about.js
@@ -0,0 +1,5 @@
+function whatCanIDrink(age){
+    if(age < 0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+}
--- spec/drink-about-specs.js
+++ spec/drink-about-specs.js
@@ -1,7 +1,7 @@
 describe("What Can I Drink", () =>{
     describe("Drinking Age is less 0", ()=>{
         it("should return an error if the age is less than 0", function(){
-            expect(whatCanIDrink).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+            expect(whatCanIDrink(-5)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         })
     })
 });
