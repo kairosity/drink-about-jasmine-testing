1b22d5395fdbf95a74d0f82a871c702b99f3858f
kairosity <karinafinegan@gmail.com>
2020-08-27 12:57:16 +0000

    Finish tests and functions

--- scripts/drink-about.js
+++ scripts/drink-about.js
@@ -1,7 +1,13 @@
 function whatCanIDrink(age){
     if(age < 0){
         return "Sorry. I can’t tell what drink because that age is incorrect!";
-    } else if (age < 14) {
-        return "Drink Toddy.";
+    } else if ((age < 14) && (age > 0)) {
+        return "Drink Water.";
+    } else if ((age < 18) && (age > 13)) {
+        return "Drink Sparkling Water.";
+    } else if ((age < 21) && (age > 17)) {
+        return "Drink Vegetable Juice.";
+    } else if ((age < 130) && (age > 21)) {
+        return "Drink Moderately.";
     }
 }
--- spec/drink-about-specs.js
+++ spec/drink-about-specs.js
@@ -1,12 +1,27 @@
 describe("What Can I Drink", () =>{
-    describe("Drinking Age is less 0", ()=>{
+    describe("Drinking Age is less than 0", ()=>{
         it("should return an error if the age is less than 0", function(){
             expect(whatCanIDrink(-5)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         })
     })
-    describe("Drinking Age is less 14", ()=>{
-        it("should return 'Drink Toddy'", function(){
-            expect(whatCanIDrink(6)).toBe("Drink Toddy.")
+    describe("Drinking Age is less than 14", ()=>{
+        it("should return 'Drink Water'", function(){
+            expect(whatCanIDrink(6)).toBe("Drink Water.")
+        })
+    })
+    describe("Drinking Age is less than 18", ()=>{
+        it("should return 'Drink Sparkling Water'", function(){
+            expect(whatCanIDrink(15)).toBe("Drink Sparkling Water.")
+        })
+    })
+    describe("Drinking Age is less than 21", ()=>{
+        it("should return 'Drink Veggie Juice'", function(){
+            expect(whatCanIDrink(20)).toBe("Drink Vegetable Juice.")
+        })
+    })
+    describe("Drinking Age is less than 130", ()=>{
+        it("should return 'Drink Moderately'", function(){
+            expect(whatCanIDrink(119)).toBe("Drink Moderately.")
         })
     })
 });
