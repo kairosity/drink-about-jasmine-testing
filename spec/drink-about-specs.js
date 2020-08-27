93a17dc194afeb074b0b328f4c87971bf7b5ae9a
kairosity <karinafinegan@gmail.com>
2020-08-27 11:54:34 +0000

    Add first test spec for age is less than 0

---
+++ spec/drink-about-specs.js
@@ -0,0 +1,7 @@
+describe("What Can I Drink", () =>{
+    describe("Drinking Age is less 0", ()=>{
+        it("should return an error if the age is less than 0", function(){
+            expect(whatCanIDrink).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        })
+    })
+});
